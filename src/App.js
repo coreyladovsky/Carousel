14af852b0999e0b734fc55a7aae1495d42ce2b50
Corey Ladovsky <coreyskymusic@gmail.com>
2018-03-23 16:37:14 -0400

    creating environment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import './App.css';
-import Picture from "./images/prod1/1.jpg";
+import React, { Component } from "react";
+import "./css/App.css";
+import Carousel from "./components/carousel";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={Picture} alt={""} />
+        <div className="title">COREY LADOVSKY SPRING 2018</div>
+        < Carousel />
       </div>
     );
   }
