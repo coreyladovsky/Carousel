956a45d8a9e2ac13fd5557ef8a2e479c786c971b
Corey Ladovsky <coreyskymusic@gmail.com>
2018-03-24 21:18:21 -0400

    back arrows now working

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,32 +1,47 @@
 import React from "react";
 import { pictures } from "../imports/imports.js";
 import CarouselList from "./carousel_list.js";
-import '../css/carousel.css';
+import "../css/carousel.css";
 
 class Carousel extends React.Component {
   constructor() {
     super();
     this.pics = pictures;
     this.state = {
-      currentPage: 0,
+      currentPage: 0
     };
     this.nextPhotos = this.nextPhotos.bind(this);
+    this.lastPhotos = this.lastPhotos.bind(this);
   }
 
   nextPhotos() {
-    this.setState({currentPage: ((this.state.currentPage + 1) % this.pics.length)});
+    this.setState({
+      currentPage: (this.state.currentPage + 1) % this.pics.length
+    });
   }
 
+  lastPhotos() {
+    let lastPhoto = this.state.currentPage - 1;
+    if (lastPhoto < 0) {
+      this.setState({ currentPage: this.pics.length - 1 });
+    } else {
+      this.setState({
+        currentPage: (this.state.currentPage - 1) % this.pics.length
+      });
+    }
+  }
 
-
-
-  render () {
-    return(
+  render() {
+    return (
       <div>
         <div className="photo-scroll-container">
-          <div className="scroll-arrow"><i className="far fa fa-angle-left angles"></i></div>
+          <div className="scroll-arrow" onClick={this.lastPhotos}>
+            <i className="far fa fa-angle-left angles" />
+          </div>
           <CarouselList pics={this.pics[this.state.currentPage]} />
-          <div className="scroll-arrow" onClick={this.nextPhotos}><i className="far fa fa-angle-right angles"></i></div>
+          <div className="scroll-arrow" onClick={this.nextPhotos}>
+            <i className="far fa fa-angle-right angles" />
+          </div>
         </div>
       </div>
     );
